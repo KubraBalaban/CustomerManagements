f619008d17b97c9417d11e64581bcd621475d7f3
Kübra <78422483+KubraBalaban@users.noreply.github.com>
2023-06-05 16:53:03 +0300

    Update AuthenticationControllerTest.cs

--- CustomerManagementApi.UnitTest/AuthenticationControllerTest.cs
+++ CustomerManagementApi.UnitTest/AuthenticationControllerTest.cs
@@ -1,30 +1,29 @@
 using JWTAuthenticationManager;
 using Microsoft.Extensions.Configuration;
+using Moq;
 
 namespace CustomerManagementApi.UnitTest
 {
     public class AuthenticationControllerTest
     {
+        private readonly Mock<IConfiguration> _configurationMock;
+
         public AuthenticationControllerTest()
         {
-           
+            _configurationMock = new Mock<IConfiguration>();
         }
         [Fact]
         public void CreateTokenTest()
         {
-            #region Arrange
             var appSettingsStub = new Dictionary<string, string> {
-                    {"CustomerUser:UserName", "CustomerUserTest"},
-                    {"CustomerUser:Password", "123456"}
+                    {"CustomerManagement:UserName", "CustomerUserTest"},
+                    {"CustomerManagement:Password", "123456"}
             };
             var configuration = new ConfigurationBuilder()
                                     .AddInMemoryCollection(appSettingsStub)
                                     .Build();
-            #endregion
-
             #region Act
-            var response = new JwtTokenHandler(configuration).GenerateJwtToken(
-                new JWTAuthenticationManager.Models.Request.AuthRequest { UserName = "User", Password = "123" });
+            var response = new JwtTokenHandler(configuration).GenerateJwtToken(new JWTAuthenticationManager.Models.Request.AuthRequest { UserName = "User", Password = "123" });
             #endregion
 
             #region Assert
